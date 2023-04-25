a52c9009c2e9cb5bad5a6ac61f43937e5f63684f
DelayedEnd <kraasm222@gmail.com>
2023-04-25 09:54:23 -0500

    Fix legacy url. BETA TEST mobile nav.

--- assets/main.js
+++ assets/main.js
@@ -58,4 +58,33 @@ $(window).on('wheel', function(event){
         }
     }
 });
+$(window).on('touchmove', function(event){
+    if (menuActive == false) return;
+    if (event.originalEvent.deltaY < 0) {
+        selected++;
+        if (selected > 10 ) selected = 1;
+        if (selected < 1) selected = 10;
+        for (i = 1; i < 11; i++) {
+            var toChange = "#menuItem" + i;
+            existingDegrees[i] -= 36;
+            console.log(selected + " " + i);
+            if (i == selected) { $(toChange).css({'background' : 'white'}); }
+            else { $(toChange).css({'background' : 'black'}); }
+            $(toChange).css({ '-webkit-transform' : 'rotate('+ existingDegrees[i] +'deg)', '-moz-transform' : 'rotate('+ existingDegrees[i] +'deg)', '-ms-transform' : 'rotate('+ existingDegrees[i] +'deg)', 'transform' : 'rotate('+ existingDegrees[i] +'deg)' });
+        }
+    }
+    else {
+        selected--;
+        if (selected > 10 ) selected = 1;
+        if (selected < 1) selected = 10;
+        for (i = 1; i < 11; i++) {
+            var toChange = "#menuItem" + i;
+            existingDegrees[i] += 36;
+            console.log(selected + " " + i);
+            if (i == selected) { $(toChange).css({'background' : 'white'}); }
+            else { $(toChange).css({'background' : 'black'}); }
+            $(toChange).css({ '-webkit-transform' : 'rotate('+ existingDegrees[i] +'deg)', '-moz-transform' : 'rotate('+ existingDegrees[i] +'deg)', '-ms-transform' : 'rotate('+ existingDegrees[i] +'deg)', 'transform' : 'rotate('+ existingDegrees[i] +'deg)' });
+        }
+    }
+});
     ////////        END NAV WHEEL        ////////
